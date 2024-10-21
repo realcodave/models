66eabc8da762bbd626516e1f7569044f478f0e68
realcodave <82022380+realcodave@users.noreply.github.com>
2024-10-21 18:29:46 +0100

    Update settings.py

--- models/settings.py
+++ models/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = STATIC_ROOT = "/opt/myproject/myproject/models/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
